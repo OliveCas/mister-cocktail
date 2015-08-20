7546c50438074b85b551bd1c7d098fc2d0bb7ebf
Olivier Casassus <olivier.casassus@gmail.com>
2015-08-20 14:37:14 +0200

    created models, routes, controllers - about to create views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
+
+
+
+# A user can see the list of all cocktails
+# GET "cocktails"
+
+
+# A user can see the details of a given cocktail, with the ingredient list
+# GET "cocktails/42"
+
+
+# A user can create a new cocktail.
+# GET "cocktails/new"
+# POST "cocktails"
+
+
+# A user can add a new dose (ingredient/description pair) on an existing cocktail.
+# GET "cocktails/42/doses/new"
+# POST "cocktails/42/doses"
+
+
+# A user can delete a dose on an existing cocktail
+# DELETE "doses/25"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
